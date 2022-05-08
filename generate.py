22d3669cfcb63d43ca3dd3986104cd660d9917ac
jchahin1121 <jchahin11@gmail.com>
2022-05-07 21:57:42 -0400

    changed sys.argv to argparse

--- generate.py
+++ generate.py
@@ -1,6 +1,10 @@
 import random
-import sys
 from string import ascii_uppercase, ascii_lowercase, digits
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-length", type=int, required=True)
+args = parser.parse_args()
 
 symbols = "!@#$%^&*()"
 chars, pw = symbols + ascii_lowercase + ascii_uppercase + digits, ""
@@ -13,6 +17,6 @@ def check(*char):
 
 
 while check(symbols, ascii_lowercase, ascii_uppercase, digits):
-    pw = ''.join(random.choices(chars, k=int(sys.argv[1])))
+    pw = ''.join(random.choices(chars, k=args.length))
 
 print(f"password: {pw}")
